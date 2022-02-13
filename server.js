e22ae8592546b3f3d5f4941b9c6522c262ffa037
lizbett <rlizbett21@gmail.com>
2022-02-13 16:39:37 -0600

    updated server.js

---
+++ server.js
@@ -0,0 +1,8 @@
+//const variables
+const express = require('express');
+const path = require('path');
+const api = require('./routes/index')
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
