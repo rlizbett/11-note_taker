e29f93466c8c3ba0c0df3d86959a94c035f2fc49
lizbett <rlizbett21@gmail.com>
2022-02-14 13:19:24 -0600

    updated server.js

--- server.js
+++ server.js
@@ -5,3 +5,20 @@ const api = require('./routes/index')
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('./pubic'));
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+    console.info(`conecting to notes, please wait a moment `)
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+    console.info(`conecting to index, please wait a moment `)
+});
+
+app.listen(PORT, () => {
+    console.log(`App listening on PORT: ${PORT}`)
+});
